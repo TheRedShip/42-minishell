afdd3977922d735df8cf09c1703c639850c0e36a
RedShip <nocoyav54@gmail.com>
2024-01-25 18:09:06 +0100

    signals weird things

--- srcs/setup/setup_signals.c
+++ srcs/setup/setup_signals.c
@@ -25,21 +25,15 @@ static void	signal_handler_inter(int signal)
 		rl_on_new_line();
 		rl_redisplay();
 	}
-	// if (signal == 3)
-	// 	printf("  \b\b");
 }
 
-static void	signal_handler_exec(int signal)
-{
-	if (signal == 2)
-		printf("^C\n");
-	else if (signal == 3)
-	{
-		g_exit_code = 131;
-		printf("^\\Quit (core dumped)\n");
-	}
-}
-
+// static void	signal_handler_exec(int signal)
+// {
+// 	if (signal == 2)
+// 		printf("^C\n");
+// 	else if (signal == 3)
+// 		printf("^\\Quit core dumped\n");
+// }
 
 void	toggle_signal(int toggle)
 {
@@ -52,7 +46,6 @@ void	toggle_signal(int toggle)
 	{
 		term_data.c_lflag = term_data.c_lflag & (~ECHOCTL);
 		tcsetattr(0, 0, &term_data);
-
 		sigemptyset(&mask);
 		sigaddset(&mask, SIGQUIT);
 		action.sa_mask = mask;
@@ -63,9 +56,10 @@ void	toggle_signal(int toggle)
 	}
 	else
 	{
-		action.sa_mask = mask;
-		action.sa_flags = 0;
-		action.sa_handler = &signal_handler_exec;
+		term_data.c_lflag = term_data.c_lflag | ECHOCTL;
+		tcsetattr(0, 0, &term_data);
+		// action.sa_handler = signal_handler_exec;
+		action.sa_handler = SIG_IGN;
 		sigaction(SIGINT, &action, NULL);
 		sigaction(SIGQUIT, &action, NULL);
 	}
