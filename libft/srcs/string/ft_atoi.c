ac43ef68d07c8085efa78f908e05738916f74f64
RedShip <nocoyav54@gmail.com>
2024-02-04 14:29:15 +0100

    using verify token in prompt

--- libft/srcs/string/ft_atoi.c
+++ libft/srcs/string/ft_atoi.c
@@ -3,40 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgramati <rgramati@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ycontre <ycontre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:38:15 by rgramati          #+#    #+#             */
-/*   Updated: 2024/02/04 13:30:20 by rgramati         ###   ########.fr       */
+/*   Updated: 2024/02/04 13:49:32 by ycontre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *nptr)
-{
-	int	i;
-	int	sign;
-	int	sum;
+// int	ft_atoi(char *nptr)
+// {
+// 	int	i;
+// 	int	sign;
+// 	int	sum;
 
-	i = 0;
-	sign = 1;
-	sum = 0;
-	while (*(nptr + i) == 32 || (*(nptr + i) >= 9 && *(nptr + i) <= 13))
-		i++;
-	if (*(nptr + i) == 43 || *(nptr + i) == 45)
-	{
-		if (*(nptr + i) == 45)
-			sign = -1;
-		i++;
-	}
-	while (*(nptr + i) >= 48 && *(nptr + i) <= 57)
-	{
-		sum *= 10;
-		sum += (*(nptr + i) - 48);
-		i++;
-	}
-	return (sign * sum);
-}
+// 	i = 0;
+// 	sign = 1;
+// 	sum = 0;
+// 	while (*(nptr + i) == 32 || (*(nptr + i) >= 9 && *(nptr + i) <= 13))
+// 		i++;
+// 	if (*(nptr + i) == 43 || *(nptr + i) == 45)
+// 	{
+// 		if (*(nptr + i) == 45)
+// 			sign = -1;
+// 		i++;
+// 	}
+// 	while (*(nptr + i) >= 48 && *(nptr + i) <= 57)
+// 	{
+// 		sum *= 10;
+// 		sum += (*(nptr + i) - 48);
+// 		i++;
+// 	}
+// 	return (sign * sum);
+// }
 
 int	ft_atoi(char *nptr)
 {
